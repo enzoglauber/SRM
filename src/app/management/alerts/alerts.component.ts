1c111a71126b63b09ab2d9ab07b778311038233d
Enzo Glauber <enzojob@gmail.com>
2018-12-03 21:38:26 -0200

    Layout

--- src/app/management/alerts/alerts.component.ts
+++ src/app/management/alerts/alerts.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-alerts',
   templateUrl: './alerts.component.html',
-  styleUrls: ['./alerts.component.scss']
+  styleUrls: ['./alerts.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AlertsComponent implements OnInit {
 
--- src/app/management/shortcuts/shortcuts.component.ts
+++ src/app/management/shortcuts/shortcuts.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-shortcuts',
   templateUrl: './shortcuts.component.html',
-  styleUrls: ['./shortcuts.component.scss']
+  styleUrls: ['./shortcuts.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ShortcutsComponent implements OnInit {
 
